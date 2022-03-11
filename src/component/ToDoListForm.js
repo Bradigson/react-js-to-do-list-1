ce397a92df044faca361ea22a3963ebdc92baa78
Bradigson <71954800+Bradigson@users.noreply.github.com>
2022-03-11 11:17:32 -0400

    iphone DONE!

--- src/component/ToDoListForm.js
+++ src/component/ToDoListForm.js
@@ -45,23 +45,24 @@ const ToDoListForm = (props)=>{
             </form>
            
         
-            <div className="accordion mt-2" id="accordionExample">
-                    <div className="accordion-item">
-                                <h2 className="accordion-header" id="headingOne">
-                                <button className="accordion-button text-center" type="button" 
-                                data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                                aria-expanded="true" aria-controls="collapseOne">
-                                    Calculator
-                                </button>
-                                </h2>
-                            <div id="collapseOne" className="accordion-collapse collapse show"    
-                                aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <Calculator/> 
-                                </div>
-                            </div>
+
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" 
+                        data-bs-toggle="collapse" data-bs-target="#collapseTwo" 
+                        aria-expanded="false" aria-controls="collapseTwo">
+                        Calculadora
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" 
+                    aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <Calculator/>   
                     </div>
-            </div>
+                </div>
+                
+           </div>
         </div>
     )
 }
